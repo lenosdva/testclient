e5f7f9aaa3f246be4bd38d9f6564b45ef1f106f1
Prashant Patil <prashant@linkites.com>
2021-02-17 01:31:33 +0530

    ui fixes

--- component/Testimonial/Testimonial.jsx
+++ component/Testimonial/Testimonial.jsx
@@ -5,7 +5,7 @@ const settings = {
   dots: true,
   infinite: true,
   arrows: true,
-  autoplay: true,
+  autoplay: false,
   speed: 500,
   autoplaySpeed: 3000,
   slidesToShow: 1,
@@ -19,28 +19,90 @@ export default function Testimonial() {
       <div className="testimonial-slider">
         <Slider {...settings}>
           <div className="full-container">
-            <div className="row">
-              <div className="col-md-3">
-                <Image
-                  src="/assets/images/howitwork2.jpg"
-                  alt="testimonial2"
-                  layout="responsive"
-                  width={228}
-                  height={228}
-                />
+            <div>
+              <div className="row">
+                <div className="col-md-3">
+                  <Image
+                    src="/assets/images/howitwork2.jpg"
+                    alt="testimonial2"
+                    layout="responsive"
+                    width={228}
+                    height={228}
+                  />
+                </div>
+                <div className="col-md-9 pl-4 d-flex flex-column justify-content-around">
+                  <p className="views">
+                    Mario Adolfo was our handyman. She went above and beyond the
+                    assigned call of duty. All of her work was first class, very
+                    quick and extremely professional. She even worked on repairing
+                    a broken window in our lawn outside in a rain storm.
+                  </p>
+                  <div>
+                    <h5 className="name mb-1">Suzzane Merkel,</h5>
+                    <h5 className="job">
+                      - A Web Developer at German IT Solutions
+                    </h5>
+                  </div>
+                </div>
               </div>
-              <div className="col-md-9 pl-4 d-flex flex-column justify-content-around">
-                <p className="views">
-                  Mario Adolfo was our handyman. She went above and beyond the
-                  assigned call of duty. All of her work was first class, very
-                  quick and extremely professional. She even worked on repairing
-                  a broken window in our lawn outside in a rain storm.
-                </p>
-                <div>
-                  <h5 className="name mb-1">Suzzane Merkel,</h5>
-                  <h5 className="job">
-                    - A Web Developer at German IT Solutions
-                  </h5>
+            </div>
+          </div>
+
+          <div className="full-container">
+            <div>
+              <div className="row">
+                <div className="col-md-3">
+                  <Image
+                    src="/assets/images/howitwork2.jpg"
+                    alt="testimonial2"
+                    layout="responsive"
+                    width={228}
+                    height={228}
+                  />
+                </div>
+                <div className="col-md-9 pl-4 d-flex flex-column justify-content-around">
+                  <p className="views">
+                    Mario Adolfo was our handyman. She went above and beyond the
+                    assigned call of duty. All of her work was first class, very
+                    quick and extremely professional. She even worked on repairing
+                    a broken window in our lawn outside in a rain storm.
+                  </p>
+                  <div>
+                    <h5 className="name mb-1">Suzzane Merkel,</h5>
+                    <h5 className="job">
+                      - A Web Developer at German IT Solutions
+                    </h5>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="full-container">
+            <div>
+              <div className="row">
+                <div className="col-md-3">
+                  <Image
+                    src="/assets/images/howitwork2.jpg"
+                    alt="testimonial2"
+                    layout="responsive"
+                    width={228}
+                    height={228}
+                  />
+                </div>
+                <div className="col-md-9 pl-4 d-flex flex-column justify-content-around">
+                  <p className="views">
+                    Mario Adolfo was our handyman. She went above and beyond the
+                    assigned call of duty. All of her work was first class, very
+                    quick and extremely professional. She even worked on repairing
+                    a broken window in our lawn outside in a rain storm.
+                  </p>
+                  <div>
+                    <h5 className="name mb-1">Suzzane Merkel,</h5>
+                    <h5 className="job">
+                      - A Web Developer at German IT Solutions
+                    </h5>
+                  </div>
                 </div>
               </div>
             </div>
