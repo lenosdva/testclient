f96266154223b486dbe736f6965595a358a3f5f7
harshitsan <harshitbhalla.hb@gmail.com>
2021-03-09 10:17:14 +0530

    add deployemnt configuration

--- next.config.js
+++ next.config.js
@@ -1,20 +1,26 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: {
-        test: /\.(js|ts)x?$/,
-      },
-      use: ['@svgr/webpack'],
-    });
-    return config;
-  },
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-  images: {
-    domains: [''],
-  }
-}
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/,
+			issuer: {
+				test: /\.(js|ts)x?$/,
+			},
+			use: ["@svgr/webpack"],
+		});
+		return config;
+	},
+	sassOptions: {
+		includePaths: [path.join(__dirname, "styles")],
+	},
+	images: {
+		loader: "imgix",
+		path: "http://deinhausmann.com.s3-website.ap-south-1.amazonaws.com/",
+	},
+	exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
+		return {
+			"/": { page: "/" },
+		}
+	},
+};
