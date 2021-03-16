2836b18c720929a3213e60851e35622e28998370
Designer <prashantpatil1255@gmail.com>
2021-03-16 10:40:37 +0530

    add modal

--- component/Navbar/Navbar.jsx
+++ component/Navbar/Navbar.jsx
@@ -77,6 +77,7 @@ const loginModal = (loginModel, closeModal, setSignUpModel, serverError) => {
   }
 
   return (
+    <div>
     <div className="modal-wrapper">
       <Modal
         isOpen={loginModel}
@@ -98,6 +99,7 @@ const loginModal = (loginModel, closeModal, setSignUpModel, serverError) => {
           <h4>Sign In</h4>
         </header>
         <div className="modalbody">
+
           {loginWith === 'phone' ?
             <form onSubmit={onLogin}>
               <div className="box">
@@ -211,6 +213,98 @@ const loginModal = (loginModel, closeModal, setSignUpModel, serverError) => {
 
         </div>
       </Modal>
+    </div>
+
+    {/* request modal open */}
+    {/* <div className="modal-wrapper">
+      <Modal
+        isOpen={loginModel}
+        onRequestClose={close}
+        ariaHideApp={false}
+        contentLabel="Example Modal"
+        className="modal-wrapper-sm modal-wrapper-md"
+      >
+        <header>
+          <button onClick={close} className="close-btn">
+            <Image
+              src="/assets/svg/close-modal.svg"
+              alt=""
+              width={24}
+              height={24}
+            />
+          </button>
+          <h4 className="blank"></h4>
+        </header>
+        <div className="modalbody">
+
+        <div className="request-modal-wrapper">
+          <div className="top-wrapper">
+            <div className="profile-pic">
+                <Image
+                  src="/assets/images/profile-pic.png"
+                  alt=""
+                  width={128}
+                  height={128}
+                />
+            </div>
+            <div className="profile-txt">
+                <h3>Service Request Sent</h3>
+                <h5>user1234 sent you a service request</h5>
+                <h6>12:57pm</h6>
+            </div>
+          </div>
+            <div className="container down-details">
+              <div className="row">
+                <div className="col-md-12">
+                  <h4>CUSTOMER DETAILS</h4>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="text-details"><span>Name:</span>Kanak Dhotre</div>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="text-details"><span>EMail:</span>dhotrekanak@gmail.com</div>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-md-6">
+                  <div className="text-details"><span>Shifting From:</span>Berlin, 400607</div>
+                </div>
+                <div className="col-md-6">
+                  <div className="text-details"><span>Shifting To:</span>Johanessburg, 400607</div>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="text-details"><span>Requested Date of Service:</span>23/12/2020</div>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="text-details"><span>Time of Service:</span>12:00pm</div>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="btn-details">
+                    <button className="outline-btn">Accept Request</button>
+                    <button className="outline-btn">Decline Request</button>
+                  </div>
+                  <div className="note"><span>Note:</span>  Dein Hausman keeps 10% of your earnings as commision</div>
+                </div>
+              </div>
+            </div>
+        </div>
+
+
+
+        </div>
+      </Modal>
+    </div> */}
+    {/* request modal close */}
     </div>
   );
 }
